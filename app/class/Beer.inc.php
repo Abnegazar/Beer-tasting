28c58001ea940527c28ab00b741352f2ac815a6c
Abnegazar <57896757+Abnegazar@users.noreply.github.com>
2021-10-01 10:40:01 +0200

    Create Beer.inc.php
    feat(controller) : Add Beer controller

---
+++ app/class/Beer.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+    class Beer{
+        private $idBeer;
+        private $style;
+        private $beerAroma;
+        private $beerAppearance;
+        private $beerFlavor;
+        private $comments;
+        private $corps;
+        private $ingredients;
+        private $styleComparison;
+        private $commercialExemple;
+        private $createdTime;
+        private $updateTime;
+    }
+
+?>
