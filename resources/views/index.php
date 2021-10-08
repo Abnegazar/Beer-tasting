74c1f71c4cc32176feb06d38abdab1dcd9a7c2fb
Ariaque <90242699+Ariaque@users.noreply.github.com>
2021-10-08 13:14:05 +0200

    First commit of frontend

---
+++ resources/views/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Tasting beer</title>
+        <?php  include('partials/link_1.php'); ?>
+    </head>
+    <body id="page-top">
+    <?php  include('partials/nav_1.php'); ?>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                        <h1 class="text-white font-weight-bold">Votre plateforme pour enregistrer vos degustations de bières</h1>
+                        <hr class="divider" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 mb-5"><strong>Tasting beer</strong> est l'ideal pour enregistrer ses degustations de bières selon les normes BJCP et consulter d'autres degustations de bieres.</p>
+                        <a class="btn btn-primary btn-xl" href="#about">Consultez les degustations</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="page-section bg-primary" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">Enregistrer votre degustation</h2>
+                        <hr class="divider divider-light" />
+                        <p class="text-white-75 mb-4"> <strong>Tasting beer</strong> offre à tous degustateurs la confidentialité et l'assurance que leurs degustations ne pourront etre modifier que par eux.</p>
+                        <a class="btn btn-light btn-xl" href="#services">S'inscrire</a>
+                    </div>
+                </div>
+            </div>
+        </section>  
+    <?php  include('partials/footer.php'); ?>
+    </body>
+</html>
---
+++ resources/views/partials/nav_1.php
@@ -0,0 +1,15 @@
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">Tasting beer</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                       <!-- <li class="nav-item"><a class="nav-link" href="#about">About</a></li>-->
+                        <li class="nav-item"><a class="nav-link" href="index.php">Degustations</a></li>
+                        <li class="nav-item"><a class="nav-link" href="login.php">Se connecter</a></li>
+                        <li class="nav-item"><a class="nav-link" href="inscription.php">S'inscrire</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
