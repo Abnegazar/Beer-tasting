e0b44deedc5da1ea6e406678588f5407eef7a372
Djedjemel Arnauld Cyriaque <arnauld-cyriaque.djedjemel@etudiant.univ-rennes1.fr>
2021-10-22 09:24:19 +0200

    chagement de reset et forgot

--- public/views/login/forgotPassword.php
+++ public/views/login/forgotPassword.php
@@ -10,23 +10,18 @@
     }
     ?>
 <!-- Alert-->
-
        <!-- Section-->
-   <div class="container">
+ <div class="container">
     <div class="row">
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <div class="card border-0 shadow rounded-3 my-5">
           <div class="card-body p-4 p-sm-5">
-            <h5 class="card-title text-center mb-5 fw-light fs-5">Modifier votre mot de passe</h5>
-            <form action="<?php echo PAGE_NEW_PASSWORD; ?>" method="POST">
+            <h5 class="card-title text-center mb-5 fw-light fs-5">Mot de passe oublié</h5>
+            <form action="<?php echo PAGE_FORGOT_PASSWORD; ?>" method="POST">
 
               <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="password">
-                <label for="floatingInput">Nouveau mot de passe</label>
-              </div>
-              <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="confirmPassword">
-                <label for="floatingInput">Confirmer le mot de passe</label>
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email">
+                <label for="floatingInput">Email address</label>
               </div>
 
               <div class="d-grid">
@@ -39,4 +34,6 @@
       </div>
     </div>
   </div>
+
+
 
