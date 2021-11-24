1bb63f7d04d499ceeb66cdf4ffb0b48343d640b0
Louis Boulle <louis.boulle@etudiant.univ-rennes1.fr>
2021-11-24 09:25:31 +0100

    update

--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,14 @@
 <?php
 
-DEFINE('PAGE_SIGNIN', '/signIn/');
-DEFINE('PAGE_SIGNUP', '/signUp/');
-DEFINE('PAGE_SIGNUP_SUCCES', '/signUp/');
-DEFINE('PAGE_LOGOUT', '/logOut/');
-DEFINE('PAGE_NEW_PASSWORD', '/newPassword/');
+DEFINE('PAGE_SIGNIN', '/signIn');
+DEFINE('PAGE_SIGNUP', '/signUp');
+DEFINE('PAGE_SIGNUP_SUCCES', '/signUp');
+DEFINE('PAGE_LOGOUT', '/logOut');
+DEFINE('PAGE_NEW_PASSWORD', '/newPassword');
 
-DEFINE('PAGE_HOME', '/home/');
+DEFINE('PAGE_HOME', '/home');
 
-DEFINE('PAGE_DASHBOARD', '/dashBoard/');
+DEFINE('PAGE_DASHBOARD', '/dashBoard');
 DEFINE('PAGE_SHOW_TASTING', '/showTasting/#id#');
-DEFINE('PAGE_DISPLAY_TASTINGS', '/displayTastings/#numberPage#')
+DEFINE('PAGE_DISPLAY_TASTINGS', '/myTastings/#numberPage#');
+DEFINE('PAGE_DISPLAY_TASTINGS', '/allUsersTastings/#numberPage#');
